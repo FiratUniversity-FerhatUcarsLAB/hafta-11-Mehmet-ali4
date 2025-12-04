2b1b74fae761d437847567d407ae89df1558fd0a
Mehmet Ali KIRAÇÇAKALI <250541069@firat.edu.tr>
2025-12-04 21:53:47 +0300

    Update Exercise4_6_Multadd.java

--- Exercise4_6_Multadd.java
+++ Exercise4_6_Multadd.java
@@ -1,16 +1,30 @@
 public class Exercise4_6_Multadd {
 
+public class Multadd {
+
     public static double multadd(double a, double b, double c) {
-        // TODO
-        return 0.0;
+        return a * b + c;
     }
 
     public static double expSum(double x) {
-        // TODO
-        return 0.0;
+        return multadd(x, Math.exp(-x), Math.sqrt(1 - Math.exp(-x)));
     }
 
     public static void main(String[] args) {
-        // TODO: test methods
+
+        System.out.println(multadd(2, 3, 4)); // test
+
+        double expr1 = multadd(Math.sin(Math.PI/4), 1,
+                               Math.cos(Math.PI/4) / 2);
+
+        double expr2 = multadd(Math.log(10), 1, Math.log(20));
+
+        System.out.println(expr1);
+        System.out.println(expr2);
+
+        double expsum = expSum(5);
+        System.out.println(expsum);
     }
+}
+
 }
